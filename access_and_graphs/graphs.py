1ee438538b8aad721eeb2fbe40cecde1ca043cfe
Daniel Whelan <dpw0990@gmail.com>
2021-12-21 01:38:54 +0000

    Began work on visualisation of data, will use docker to host pseudo website to display application

--- access_and_graphs/graphs.py
+++ access_and_graphs/graphs.py
@@ -10,6 +10,44 @@ commits = p.read_csv("Users/dwhelan/Desktop/Computer Science/Sweng/Github-API-Ac
 
 
 
-commit_graph = p.DataFrame(p.merge(repos,commits,how='left',left_on='ID',right_on='Repo ID')).groupby('ID').size().reset_index()
-commit_graph.columns(['ID', 'Commits'])
+commit_number = p.DataFrame(p.merge(repos,commits,how='left',left_on='ID',right_on='Repo ID')).groupby('ID').size().reset_index()
+commit_number.columns(['ID', 'Commits'])
+
+combo = p.merge(repos,commit_number,on = 'ID')
+repos_info = combo['Name']
+commit_info = combo['Commit Number']
+list_of_langs = []
+for l in combo['Languages']:
+    if type(l) is str:
+        for lang in l.split(','):
+            list_of_langs.append(lang.strip())
+lang_count = p.Series(list_of_langs).value_counts
+
+
+title = {
+    "font-weight" : "bold",
+    "text-align" : "center",
+}
+subheading = {
+    "font-style" : "italic",
+    "text-align" : "center",
+}
+
+app = dash.Dash(__name__)
+app_layout = html.Div(children=[
+    html.H1("Github Software Engineering Measurement Graph", style=title),
+    html.H2("A Visualisation of " + repos['Owner'][0] + "'s Github Data"),
+
+    html.Div(
+        html.Div(
+            core.Graph(
+                
+            )
+        )
+    )
+])
+
+
+
+    
 
